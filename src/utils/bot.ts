86c1a76df336c129e06f6232f4dfac017bdbad07
hyperbola <me@hyperbola.me>
2022-10-23 18:39:50 +0800

    feat: merge util/cookie ito util/bot

--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs'
-import { BotCredential, parsePlainCookie } from './cookie'
 import { mergeMerchants } from './merchants'
+import { InvalidCookieError } from '../lang/errors'
 import { OfferListFollowResult, SearchedOffer } from '../lang/offer'
 import { IShopbackBot, ShopbackBot } from '../shopback-bot'
 
@@ -23,6 +23,47 @@ export async function followOffersByKeyword(
   }
 
   return searchList as OfferListFollowResult
+}
+
+export interface BotCredential {
+  refreshToken: string
+  accessToken: string
+  clientUserAgent: string
+}
+
+export function parsePlainCookie(cookieStr: string): BotCredential {
+  const firstLine = cookieStr.split('\n')[0].trim()
+  const cookies = firstLine.split(';')
+
+  let accessToken = ''
+  let refreshToken = ''
+  let clientUserAgent = ''
+  for (let cookie of cookies) {
+    cookie = cookie.trim()
+    const indexEq = cookie.indexOf('=')
+    if (indexEq === -1) {
+      continue
+    }
+    const key = cookie.substring(0, indexEq)
+    const value = cookie.substring(indexEq + 1)
+    switch (key) {
+      case 'authDeviceId':
+        clientUserAgent = value
+        break
+      case 'sbet':
+        accessToken = value
+        break
+      case 'sbrefresh': // cspell:disable-line
+        refreshToken = value
+        break
+    }
+  }
+
+  if (accessToken && refreshToken && clientUserAgent) {
+    return { accessToken, refreshToken, clientUserAgent }
+  }
+
+  throw new InvalidCookieError(cookieStr)
 }
 
 export function buildBotFromCredential(credPath: string): IShopbackBot {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from './bot'
-export * from './cookie'
 export * from './merchants'

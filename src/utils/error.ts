786b6497932d29d7be22ed1ad07f7b9ae524b31c
hyperbola <me@hyperbola.me>
2022-10-25 00:30:18 +0800

    feat: elaborate network error handling

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -8,6 +8,29 @@ import {
 } from '../lang/errors'
 import * as ExitCode from '../lang/exit-code'
 
+function handleNetworkError(e: AxiosError): never {
+  logger.debug('Status code: ' + stringify(e.status))
+  logger.debug('Shopback server response: ' + stringify(e.response?.data))
+
+  const data: any = e.response?.data
+
+  if (data?.error?.message) {
+    // Shopback server gives regular error message.
+    logger.error('Shopback server: ' + data.error.message)
+    process.exit(ExitCode.API_ERROR)
+  }
+
+  if (data) {
+    // Shopback server gives irregular error data.
+    logger.error(`Shopback server: ${stringify(data)}`)
+    process.exit(ExitCode.API_ERROR)
+  }
+
+  // Unknown network error.
+  logger.error('Network error: ' + e.message)
+  process.exit(ExitCode.NETWORK_ERROR)
+}
+
 export function handleError(e: unknown): never {
   if (e instanceof InvalidCredentialFileException) {
     logger.error(e.message)
@@ -25,13 +48,7 @@ export function handleError(e: unknown): never {
   }
 
   if (e instanceof AxiosError) {
-    logger.error(
-      'Failed to communicate with Shopback server: ' +
-        stringify(e.response?.data)
-    )
-    logger.debug('Status code: ' + stringify(e.status))
-    logger.debug('Shopback server response: ' + stringify(e.response?.data))
-    process.exit(ExitCode.API_ERROR)
+    handleNetworkError(e)
   }
 
   if (e instanceof Error) {
